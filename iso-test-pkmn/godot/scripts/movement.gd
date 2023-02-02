733247c480000dc9592fbb2156a4c9afaf574ba1
Cory Parsnipson <cparsnipson@gmail.com>
2023-02-02 00:53:38 -0800

    Implement iso grid aligned movement in iso-test-pkmn.
    Don't bump into anything. Collisions not handled yet.
    
    Also needs a refactor to separate all the different things happening in
    _fixed_process to be less error prone.

--- iso-test-pkmn/godot/scripts/movement.gd
+++ iso-test-pkmn/godot/scripts/movement.gd
@@ -1,13 +1,16 @@
 extends KinematicBody2D
 
+enum PlayerState { STAND, WALK, TURN }
 enum Direction { NORTH, NORTH_WEST, WEST, SOUTH_WEST, SOUTH, SOUTH_EAST, EAST, NORTH_EAST }
 enum DirectionString { N, NW, W, SW, S, SE, E, NE }
 
 var SPEED = 50
-var motion = Vector2(0, 0)
+var state = PlayerState.STAND
 var velocity = Vector2(0, 0)
 var prev_velocity = Vector2(INF, INF)
+var destination = Vector2(0, 0)
 var direction = Direction.SOUTH_WEST
+var tilemap = null
 onready var animation_player = $AnimationPlayer
 
 func cartesian_to_isometric(cartesian):
@@ -37,30 +40,73 @@ func vector_to_direction(vector: Vector2):
 			direction = Direction.NORTH_EAST
 
 
+func get_nearest_tilemap():
+	var map = get_parent()
+	while map:
+		if map is TileMap:
+			break
+		map = map.get_parent()
+		
+	return map
+
+
+func snap_to_tilemap(map, position = global_position):
+	if not map:
+		print("WARNING: align_to_tilemap provided invalid map")
+		return
+
+	var tile_pos = map.world_to_map(position)
+	var world_pos = map.map_to_world(tile_pos)
+	
+	global_position = world_pos - $player_sprite.offset
+
+
+func _ready():
+	tilemap = get_nearest_tilemap()
+	if tilemap:
+		snap_to_tilemap(tilemap)
+
+
 func _physics_process(delta):
 	var anim = ""
 	var anim_suffix = ""
 	
-	velocity = Vector2(0, 0)
-	
-	if Input.is_action_pressed("up"):
-		velocity += Vector2(0, -1)
-	
-	if Input.is_action_pressed("left"):
-		velocity += Vector2(-1, 0)
+	if state == PlayerState.STAND:
+		velocity = Vector2(0, 0)
+		if Input.is_action_pressed("up"):
+			velocity += Vector2(0, -1)
 		
-	if Input.is_action_pressed("right"):
-		velocity += Vector2(1, 0)
+		if Input.is_action_pressed("left"):
+			velocity += Vector2(-1, 0)
+			
+		if Input.is_action_pressed("right"):
+			velocity += Vector2(1, 0)
+			
+		if Input.is_action_pressed("down"):
+			velocity += Vector2(0, 1)
 		
-	if Input.is_action_pressed("down"):
-		velocity += Vector2(0, 1)
+		if velocity != Vector2():
+			anim = "walk"
+			state = PlayerState.WALK
+			destination = tilemap.world_to_map(global_position) + velocity
+		else:
+			anim = "stand"
+			state = PlayerState.STAND
 	
-	motion = velocity.normalized() * SPEED * delta
+	var motion = velocity.normalized() * SPEED * delta
 	motion = cartesian_to_isometric(motion)
-	move_and_collide(motion)
+	
+	var dest_position = tilemap.map_to_world(destination) + $player_sprite.offset
+	var remaining_length = dest_position - global_position
+	
+	if remaining_length.length() <= motion.length():
+		motion = remaining_length
+		anim = "stand"
+		state = PlayerState.STAND
+	
+	move_and_collide(motion) # TODO: handle collision
 	
 	vector_to_direction(velocity)
-
 	match (direction):
 		Direction.NORTH:
 			anim_suffix = "ne"
@@ -78,11 +124,6 @@ func _physics_process(delta):
 			anim_suffix = "se"
 		Direction.SOUTH_WEST:
 			anim_suffix = "sw"
-
-	if velocity.length() == 0:
-		anim = "stand"
-	else:
-		anim = "walk"
 
 	if prev_velocity != velocity:
 		$AnimationPlayer.play(anim + "-" + anim_suffix);
