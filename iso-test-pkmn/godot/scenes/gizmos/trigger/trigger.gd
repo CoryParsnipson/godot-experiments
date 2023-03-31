8aaaf81c0d0a3b1295daa106fe785002f95c8d00
Cory Parsnipson <cparsnipson@gmail.com>
2023-03-30 18:06:54 -0700

    Fix bug in trigger gizmo when disabling warnings
    The logic was wrong. Nest the logic so undefined interface method with
    disabled warnings combination do not cause invalid index access.

--- iso-test-pkmn/godot/scenes/gizmos/trigger/trigger.gd
+++ iso-test-pkmn/godot/scenes/gizmos/trigger/trigger.gd
@@ -10,8 +10,9 @@ func _ready():
 
 func _on_trigger_body_entered(body):
 	var action = _action if _action else owner
-	if not action.has_method("on_trigger_entered") and show_uninitialized_warning:
-		print("[WARNING] trigger.entered on (%s): action does not have on_trigger_entered defined." % owner.name)
+	if not action.has_method("on_trigger_entered"):
+		if show_uninitialized_warning:
+			print("[WARNING] trigger.entered on (%s): action does not have on_trigger_entered defined." % owner.name)
 		return
 		
 	action.on_trigger_entered(body)
@@ -19,8 +20,9 @@ func _on_trigger_body_entered(body):
 
 func _on_trigger_body_exited(body):
 	var action = _action if _action else owner
-	if not action.has_method("on_trigger_exited") and show_uninitialized_warning:
-		print("[WARNING] trigger.exited on (%s): action does not have on_trigger_exited defined." % owner.name)
+	if not action.has_method("on_trigger_exited"):
+		if show_uninitialized_warning:
+			print("[WARNING] trigger.exited on (%s): action does not have on_trigger_exited defined." % owner.name)
 		return
 		
 	action.on_trigger_exited(body)
