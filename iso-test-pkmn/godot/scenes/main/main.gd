0dc7d3c904dd47d49e03a08f5effd8608cab533c
Cory Parsnipson <cparsnipson@gmail.com>
2023-04-08 23:26:56 -0700

    Change main scene to load initial scene in code.

--- iso-test-pkmn/godot/scenes/main/main.gd
+++ iso-test-pkmn/godot/scenes/main/main.gd
@@ -4,6 +4,7 @@ export (NodePath) onready var level_root = get_node(level_root)
 export (NodePath) onready var screen_wipe = get_node(screen_wipe)
 export (NodePath) onready var dialogue_manager = get_node(dialogue_manager)
 
+export (String, FILE) var initial_scene
 
 func _ready():
 	# initialize items from config settings
@@ -13,3 +14,10 @@ func _ready():
 	game.level_root = level_root
 	game.screen_wipe = screen_wipe
 	game.dialogue = dialogue_manager
+
+	# load initial scene
+	var initial_scene_inst = load(initial_scene).instance()
+	if not initial_scene_inst is Level:
+		print("[ERROR] main.ready: initial scene does not point to valid Level object. (%s)" % initial_scene_inst)
+		return
+	game.push_scene(initial_scene_inst)
