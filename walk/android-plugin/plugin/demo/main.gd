04d6bc5a84e6cc288368ef031908a7ad8464297c
Cory Parsnipson <cparsnipson@gmail.com>
2024-05-07 00:34:41 -0700

    Rename android-plugin and customize to current usage.

--- walk/android-plugin/plugin/demo/main.gd
+++ walk/android-plugin/plugin/demo/main.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-# TODO: Update to match your plugin's name
-var _plugin_name = "GodotAndroidPluginTemplate"
+var _plugin_name = "GodotStepsCounterPlugin"
 var _android_plugin
 
 func _ready():
 	if Engine.has_singleton(_plugin_name):
 		_android_plugin = Engine.get_singleton(_plugin_name)
+		print("%s successfully loaded!" % _plugin_name)
 	else:
 		printerr("Couldn't find plugin " + _plugin_name)
 
@@ -14,3 +14,4 @@ func _on_Button_pressed():
 	if _android_plugin:
 		# TODO: Update to match your plugin's API
 		_android_plugin.helloWorld()
+		print("button pressed!")
